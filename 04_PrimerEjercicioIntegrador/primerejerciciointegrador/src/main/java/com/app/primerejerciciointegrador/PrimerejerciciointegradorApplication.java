c6e7b04852be03186110356cec762009d91eb9c3
Jesús Cuautle <j.cuautle016@gmail.com>
2026-05-31 09:58:50 -0600

    feat: Agrega proyecto de primer ejercicio integrador

---
+++ 04_PrimerEjercicioIntegrador/primerejerciciointegrador/src/main/java/com/app/primerejerciciointegrador/PrimerejerciciointegradorApplication.java
@@ -0,0 +1,13 @@
+package com.app.primerejerciciointegrador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimerejerciciointegradorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimerejerciciointegradorApplication.class, args);
+	}
+
+}
