53ffb32b78c40deda591e34294fa4a261d9dc767
Jesús Cuautle <j.cuautle016@gmail.com>
2026-05-31 08:44:44 -0600

    feat: Actualiza controller con endpoints personalizados con requestparam

--- 03_HelloAPI/helloapi/src/main/java/com/app/helloapi/controller/HelloApiController.java
+++ 03_HelloAPI/helloapi/src/main/java/com/app/helloapi/controller/HelloApiController.java
@@ -1,6 +1,7 @@
 package com.app.helloapi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +10,20 @@ public class HelloApiController {
     @GetMapping
     public String helloApi(){
         return "Hello API!\nMi primera API";
+    }
+    
+    @GetMapping("hello/requestparam")
+    public String helloRequestParam(@RequestParam String nombre){
+        return "Hello " + nombre + ", con RequestParam";
+    }
+    
+    @GetMapping("bye/requestparam")
+    public String byeRequestParam(
+            @RequestParam String nombre,
+            @RequestParam int edad,
+            @RequestParam String profesion
+    ){
+        return "Bye!\nNombre: " + nombre + "\nEdad: " + edad + "\nProfesion: " + profesion;
     }
     
 }
