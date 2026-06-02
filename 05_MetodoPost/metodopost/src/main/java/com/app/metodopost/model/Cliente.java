0dd843f5c2942ad5ae4b800094b3bd420a6fb113
Jesús Cuautle <j.cuautle016@gmail.com>
2026-06-01 20:57:32 -0600

    feat: Agrega constructor a clase cliente

--- 05_MetodoPost/metodopost/src/main/java/com/app/metodopost/model/Cliente.java
+++ 05_MetodoPost/metodopost/src/main/java/com/app/metodopost/model/Cliente.java
@@ -1,10 +1,12 @@
 package com.app.metodopost.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class Cliente {
     
     private long id;
