d2aa72b0cc557b809ef3af3421608894faada44d
Jesús Cuautle <j.cuautle016@gmail.com>
2026-06-01 19:46:03 -0600

    feat: Agrega proyecto base creado en spring initializr

---
+++ 05_MetodoPost/metodopost/src/test/java/com/app/metodopost/MetodopostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.metodopost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetodopostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
