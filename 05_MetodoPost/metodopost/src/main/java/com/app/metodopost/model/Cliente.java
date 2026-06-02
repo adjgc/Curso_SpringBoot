7db5cb48a9242d77461ad6b610f3235d42f18c7a
Jesús Cuautle <j.cuautle016@gmail.com>
2026-06-01 20:26:29 -0600

    feat: Agrega clase cliente

---
+++ 05_MetodoPost/metodopost/src/main/java/com/app/metodopost/model/Cliente.java
@@ -0,0 +1,14 @@
+package com.app.metodopost.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Cliente {
+    
+    private long id;
+    private String nombre;
+    private String apellido;
+    
+}
