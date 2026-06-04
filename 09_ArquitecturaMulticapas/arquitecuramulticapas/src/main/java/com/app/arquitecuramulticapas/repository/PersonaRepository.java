605b4873bdfd8f35c9904af4e824cb962798bb81
Jesús Cuautle <j.cuautle016@gmail.com>
2026-06-03 21:02:37 -0600

    feat: Agrega clase de repository de persona

---
+++ 09_ArquitecturaMulticapas/arquitecuramulticapas/src/main/java/com/app/arquitecuramulticapas/repository/PersonaRepository.java
@@ -0,0 +1,14 @@
+package com.app.arquitecuramulticapas.repository;
+
+import com.app.arquitecuramulticapas.model.Persona;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class PersonaRepository {
+    
+    //Metodos de consulta de registros en la base de datos
+    public Persona consultarPersona(long id){
+        return null;
+    }
+    
+}
