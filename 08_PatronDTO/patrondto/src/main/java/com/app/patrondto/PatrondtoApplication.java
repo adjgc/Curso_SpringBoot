c1c6798c38ca114d4d0e144c1a01f306b12b459e
Jesús Cuautle <j.cuautle016@gmail.com>
2026-06-03 19:22:08 -0600

    feat: Agrega proyecto base creado con spring initializr

---
+++ 08_PatronDTO/patrondto/src/main/java/com/app/patrondto/PatrondtoApplication.java
@@ -0,0 +1,13 @@
+package com.app.patrondto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatrondtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatrondtoApplication.class, args);
+	}
+
+}
