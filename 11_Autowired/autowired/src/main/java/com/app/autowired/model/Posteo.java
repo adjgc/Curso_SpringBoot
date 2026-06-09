1a2dad7dd7184b25a0fbecb0e1ed1d6b511f5f7d
Jesús Cuautle <j.cuautle016@gmail.com>
2026-06-08 19:57:53 -0600

    feat: Agrega clase posteo

---
+++ 11_Autowired/autowired/src/main/java/com/app/autowired/model/Posteo.java
@@ -0,0 +1,18 @@
+package com.app.autowired.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Posteo {
+    
+    private Long id;
+    private String titulo;
+    private String autor;
+    
+}
