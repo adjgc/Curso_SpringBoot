6977088f09fc9f7eb459afc43f7894b4b5266f06
Jesús Cuautle <j.cuautle016@gmail.com>
2026-06-09 19:33:53 -0600

    feat: Agrega proyecto base

---
+++ 14_SextoEjercicioIntegrador/sextoejerciciointegrador/src/main/java/com/app/sextoejerciciointegrador/SextoejerciciointegradorApplication.java
@@ -0,0 +1,13 @@
+package com.app.sextoejerciciointegrador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SextoejerciciointegradorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SextoejerciciointegradorApplication.class, args);
+	}
+
+}
---
+++ 14_SextoEjercicioIntegrador/sextoejerciciointegrador/src/test/java/com/app/sextoejerciciointegrador/SextoejerciciointegradorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.sextoejerciciointegrador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SextoejerciciointegradorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
