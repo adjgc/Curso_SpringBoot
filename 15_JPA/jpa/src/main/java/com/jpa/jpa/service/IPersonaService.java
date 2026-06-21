d43f487264be44903660dad148ce80aaa479e3f2
Jesús Cuautle <j.cuautle016@gmail.com>
2026-06-21 13:27:10 -0600

    feat: Agrega interface service de persona

---
+++ 15_JPA/jpa/src/main/java/com/jpa/jpa/service/IPersonaService.java
@@ -0,0 +1,18 @@
+package com.jpa.jpa.service;
+
+import com.jpa.jpa.model.Persona;
+import java.util.List;
+
+public interface IPersonaService {
+    
+    public List<Persona> getPersonas();
+    
+    public void savePersona(Persona persona);
+    
+    public void deletePersona(Long id);
+    
+    public Persona getPersona(Long id);
+    
+    public void editPersona(Long idOriginal, Long idNuevo, String nuevoNombre, String nuevoApellido, Integer nuevaEdad);
+    
+}
