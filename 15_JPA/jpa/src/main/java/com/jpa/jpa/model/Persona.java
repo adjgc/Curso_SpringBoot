b71acb0a5296ec267c3fe0ed0880cb908d522bc4
Jesús Cuautle <j.cuautle016@gmail.com>
2026-06-22 20:20:43 -0600

    feat: Actualiza clase persona con relacion one-to-one

--- 15_JPA/jpa/src/main/java/com/jpa/jpa/model/Persona.java
+++ 15_JPA/jpa/src/main/java/com/jpa/jpa/model/Persona.java
@@ -4,11 +4,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Persona {
     
@@ -18,5 +24,8 @@ public class Persona {
     private String nombre;
     private String apellido;
     private Integer edad;
+    @OneToOne
+    @JoinColumn(name="mascota_id_mascota", referencedColumnName = "id_mascota")
+    private Mascota mascota;
     
 }
