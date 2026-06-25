b7e5b20764f9477f41dbf65cf45eab53fba586db
Jesús Cuautle <j.cuautle016@gmail.com>
2026-06-24 21:01:36 -0600

    feat: Agrega implementacion de service de tema

--- 16_SeptimoEjercicioIntegrador/cursos/src/main/java/com/app/cursos/service/ITemaService.java
+++ 16_SeptimoEjercicioIntegrador/cursos/src/main/java/com/app/cursos/service/ITemaService.java
@@ -1,8 +1,16 @@
 package com.app.cursos.service;
 
+import com.app.cursos.model.Curso;
+import com.app.cursos.model.Tema;
+import java.util.List;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface ITemaService {
+    
+    public Tema getTema(Long id_tema);
+    public Tema saveTema(Tema tema);
+    public List<Tema> getTemasFromCurso(Curso curso);
+    public Tema editTema(Tema tema);
     
 }
