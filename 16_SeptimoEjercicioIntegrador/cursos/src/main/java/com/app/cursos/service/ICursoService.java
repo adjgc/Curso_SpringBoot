506ece5efae86daee5db26736535cc369e8233a6
Jesús Cuautle <j.cuautle016@gmail.com>
2026-06-24 20:16:29 -0600

    feat: Agrega service sin implementacion

---
+++ 16_SeptimoEjercicioIntegrador/cursos/src/main/java/com/app/cursos/service/ICursoService.java
@@ -0,0 +1,8 @@
+package com.app.cursos.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface ICursoService {
+    
+}
---
+++ 16_SeptimoEjercicioIntegrador/cursos/src/main/java/com/app/cursos/service/TemaService.java
@@ -0,0 +1,10 @@
+package com.app.cursos.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class TemaService implements ITemaService {
+    
+}
