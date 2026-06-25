bf2594db03260951e4937949dc6976eaffc3fdd3
Jesús Cuautle <j.cuautle016@gmail.com>
2026-06-24 20:16:55 -0600

    feat: Agrega controller base sin definiciones

---
+++ 16_SeptimoEjercicioIntegrador/cursos/src/main/java/com/app/cursos/controller/CursoController.java
@@ -0,0 +1,10 @@
+package com.app.cursos.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class CursoController {
+    
+}
